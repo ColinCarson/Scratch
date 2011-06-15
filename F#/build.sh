cb63b751b8ecc9b3bece2ff326a072c2064e4eca
Colin Carson <colincarson@gmail.com>
2011-06-16 00:15:46 +0100

    New F# stuff

---
+++ F#/build.sh
@@ -0,0 +1,3 @@
+# FSharp build script
+echo "Building FSharp file: test.fs"
+fsc test.fs --out:/Users/Colin/Projects/build/test.exe
---
+++ F#/test.fs
@@ -0,0 +1,9 @@
+#light
+
+let halfway a b =
+ let dif = b - a
+ let mid = dif / 2
+ mid + a
+
+printfn "(halfway 5 11) = %i" (halfway 5 11)
+printfn "(halfway 11 5) = %i" (halfway 11 5)
